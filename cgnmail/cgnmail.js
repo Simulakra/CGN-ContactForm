dd83d22820773ba245ac0e784166d02cf87694be
Berke D <tr.berkedundar@gmail.com>
2019-02-13 14:57:51 +0300

    sweet alert ile bildirim

--- cgnmail/cgnmail.js
+++ cgnmail/cgnmail.js
@@ -3,6 +3,12 @@ $.ajax({
 url:'cgnmail/captcha_get.php',
 success:function(data)
 { 
+    swal({
+     title: "Gönderiliyor",
+     text: "Mesajınız bize ulaştırılıyor...",
+     type: "success",
+     timer: 3000
+     });
     var truecaptcha = data; 
     var camecaptcha = $("#captcha").val();
     //captcha input id'si
@@ -15,7 +21,7 @@ success:function(data)
     if( !$("#userEmail").val() ){ $("#userEmail").attr('placeholder','Bu Alan Zorunludur.'); empty_one = true; }
     if( !$("#subject").val() ){ $("#subject").attr('placeholder','Bu Alan Zorunludur.'); empty_one = true; }
     if( !$("#content").val() ){ $("#content").attr('placeholder','Bu Alan Zorunludur.'); empty_one = true; }
-    if(empty_one){ alert("Zorunlu Alanlari Doldurunuz"); return; }
+    if(empty_one){ alert("Zorunlu Alanları Doldurunuz"); return; }
 
     var mailBody = "CGN Mail Form";
     mailBody += "<br>Name: " + $("#userName").val();
@@ -32,11 +38,12 @@ success:function(data)
     To : 'to@mail.com',
     Subject : "This is the subject",
     Body : mailBody
-    }).then(message => alert(message));
+    }).then(message => mailResult(message));
 
     }
     else{
-        alert("CAPTCHA HATASI\nTekrar Deneyin");
+        //"warning", "error", "success", "info"
+        swal("Captcha Hatası!", "Lütfen Tekrar Deneyin!", "error");
         refreshCaptcha();
     }
 }
@@ -45,4 +52,12 @@ success:function(data)
 
 function refreshCaptcha() {
     $("#captcha_code").attr('src','cgnmail/captcha_code.php');
+}
+
+function mailResult(message) {
+    if(message=="OK")
+        swal("Gönderildi", "Mesajınız Bize Ulaştı", "success");
+    else
+        swal("Mail Gönderme Hatası Oluştu", "Mesaj:\n"+message, "warning");
+
 }
